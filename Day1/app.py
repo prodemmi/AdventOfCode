1c8ce420dfb3ebf643232a219d283e74a527611c
Emad Malekpour <prodemmi@gmail.com>
2024-02-02 00:21:13 +0330

    done day one

--- Day1/app.py
+++ Day1/app.py
@@ -1,24 +1,53 @@
+import os
+import time
+
+start = time.time()
+
 mapNumbers = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight  ': 8,
-    'nine': 9
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
 }
 
-with open('input.txt', 'r') as file:
+def checkCharStackIsInNumbers(stack: str):
+    for number in mapNumbers.keys():
+        if number in stack:
+            return mapNumbers.get(number)
+        
+    return None
+
+def detectNumber(line: str, reversed: bool = False):
+    charStack= ''
+    for char in line:
+        char = str(char)
+        if reversed:
+            charStack = char + charStack
+        else:
+            charStack += char
+
+        if char.isnumeric():
+            return char
+        
+        isInNumbers = checkCharStackIsInNumbers(charStack)
+        if isInNumbers:
+            return isInNumbers
+
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as file:
     sum = 0
     for line in file.readlines():
-        lineNumber = ''
-        for i, (key, value) in enumerate(mapNumbers.items()):
-            line = line.replace(key, str(value))
-        for char in line:
-            if str(char).isnumeric():
-                lineNumber += char
-        sum += int(lineNumber[0] + lineNumber[-1])
+            line = line.strip()
+            first = detectNumber(line)
+            second = detectNumber(line[::-1], True)
+            sum += int(first + second)
 
     print(sum)
+
+end = time.time()
+
+print(end - start)
